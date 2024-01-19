91416074c5be5067f0706f9c3c4750d0985efd1a
Simon Ser <contact@emersion.fr>
2024-01-19 21:14:48 +0100

    Ensure here-document doesn't get variable substitution

--- init-postgres.sh
+++ init-postgres.sh
@@ -2,7 +2,7 @@
 
 createdb meta.sr.ht
 psql -d meta.sr.ht </data/meta.sr.ht.sql
-psql -d meta.sr.ht <<EOF
+psql -d meta.sr.ht <<'EOF'
 INSERT INTO "user" (id, created, updated, username, password, email, user_type)
 VALUES (1, NOW(), NOW(), 'root', '$2b$12$mlcAuUeU5reidsdPcZm/kOR4EWkZE2lbMvyVSHLpDuzDtm6MGn0Ne', 'root@localhost', 'admin');
 EOF
