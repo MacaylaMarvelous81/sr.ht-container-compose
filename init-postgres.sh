60bb3a690de04996d379f09c988cbab04aa78924
Simon Ser <contact@emersion.fr>
2024-01-19 22:40:37 +0100

    Document how to re-run init-postgres script

--- init-postgres.sh
+++ init-postgres.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -eu
 
+# Note: this script is only run once when the postgres volume is created. To
+# run it again:
+#
+#     docker volume rm srht-containers_postgres-data
+
 password='$2b$12$mlcAuUeU5reidsdPcZm/kOR4EWkZE2lbMvyVSHLpDuzDtm6MGn0Ne'
 client_secret='3eeeef2fd9d1e0176a50ee76d3382f7211231b832852bdfc3569a3bb5512158a19b1262b73ea735059004fd3ded53303e42f605e68ccbc9d3af84a373331aa9d'
 createdb meta.sr.ht
